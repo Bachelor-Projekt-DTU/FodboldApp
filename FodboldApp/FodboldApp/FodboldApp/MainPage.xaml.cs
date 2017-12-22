99050f437caae272415d0eb21d3c9c65caa2e63f
Hadi <hadihorani@hotmail.com>
2017-12-22 18:56:52 +0100

    Titel, listview med indhold og knap tilføjet

--- FodboldApp/FodboldApp/FodboldApp/MainPage.xaml.cs
+++ FodboldApp/FodboldApp/FodboldApp/MainPage.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace FodboldApp
@@ -12,6 +9,19 @@ namespace FodboldApp
 		public MainPage()
 		{
 			InitializeComponent();
-		}
+
+            // Sætter data i listviewet
+            clubList.ItemsSource = new string[]{"BK Frem", "Klub 2", "Klub 3", "Klub 4", "Klub 5"};
+
+            // Sætter farven til linjen mellem items i listviewt
+            clubList.SeparatorColor = Color.Black;
+
+            /* Listviewet skal reduceres, da der ellers vil være et tomt rum i listviewet. 
+            Listviewet indeholder et gridview som skabelon, og det har to 2 rækker. Det indeholder et billede.
+            Dvs. højden, er typisk, af et item i listviewet er det samme som højden af billedet. 
+            For at få listviewt til at tilpasse sig siger vi: HeightRequest = (#items * højden af et item) +(Standard højden + #items)*/
+
+            clubList.HeightRequest = (5 * clubList.RowHeight) + (10 * 22.5);
+        }
 	}
 }
