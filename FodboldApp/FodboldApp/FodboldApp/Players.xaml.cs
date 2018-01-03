7a16db41140e44eb433f8f3315959a4c9896efb1
Hadi <hadihorani@hotmail.com>
2018-01-03 16:17:00 +0100

    1.Holdet page is partially implemented - Missing player images in shape of an ellipse, rest of the team and navigation for each player. ScrollView in App.xaml is fixed.

--- FodboldApp/FodboldApp/FodboldApp/Players.xaml.cs
+++ FodboldApp/FodboldApp/FodboldApp/Players.xaml.cs
@@ -15,6 +15,21 @@ namespace FodboldApp
 		public Players ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            //Add tap handler to grid_Element0:
+            TapGestureRecognizer tapEvent = new TapGestureRecognizer();
+
+            //Connect event handler (Players_Tapped) to TapGestureRecognizer:
+            tapEvent.Tapped += Players_Tapped;
+
+            //Add TapGestureRecognizer to grid_Element0:
+            grid_Element0.GestureRecognizers.Add(tapEvent);
+        
+
+        void Players_Tapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("Players_Tapped");
+        }
+    }
+    }
 }
