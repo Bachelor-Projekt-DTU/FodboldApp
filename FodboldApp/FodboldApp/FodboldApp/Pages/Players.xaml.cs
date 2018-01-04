e9e5c473891cabae1629d61cd5ba788d081920c9
Olivier Bonde Petersen <osbpetersen@gmail.com>
2018-01-04 01:25:52 +0100

    Players almost done

--- FodboldApp/FodboldApp/FodboldApp/Pages/Players.xaml.cs
+++ FodboldApp/FodboldApp/FodboldApp/Pages/Players.xaml.cs
@@ -14,24 +14,13 @@ namespace FodboldApp
         public Players()
         {
             InitializeComponent();
+        }
 
-            //Add tap handler to grid_Element0:
-            TapGestureRecognizer tapEvent = new TapGestureRecognizer();
+        async void Players_Tapped(object sender, EventArgs e)
+        {
+            await ((App)App.Current).MainPage.Navigation.PushAsync(new Player_Description());
 
-            //Connect event handler (Players_Tapped) to TapGestureRecognizer:
-            tapEvent.Tapped += Players_Tapped;
-
-            //Add TapGestureRecognizer to grid_Element0:
-            grid_Element0.GestureRecognizers.Add(tapEvent);
-
-
-            async
-            void Players_Tapped(object sender, EventArgs e)
-            {
-            await ((App) App.Current).MainPage.Navigation.PushAsync(new Player_Description ());
-   
-                Console.WriteLine("Players_Tapped");
-            }
+            Console.WriteLine("Players_Tapped");
         }
     }
 }
