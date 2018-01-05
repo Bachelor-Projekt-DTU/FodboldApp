6a16665e182cb969c88b2ad211fffce044046f7d
Olivier Bonde Petersen <osbpetersen@gmail.com>
2018-01-05 16:31:56 +0100

    Dynamic lists altered to automize slightly better, PlayersData supports data for player descriptions, Playersdescription started, Coloring lists moved to its own class, Category created to setup design data, Data design for player description made

---
+++ FodboldApp/FodboldApp/FodboldApp/BoxDesigns/PlayerDescriptionDesign.cs
@@ -0,0 +1,48 @@
+﻿using FodboldApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FodboldApp.BoxDesigns
+{
+    class PlayerDescriptionDesign
+    {
+        string Name { get; set; }
+        string Sponsor { get; set; }
+        string Position { get; set; }
+        string Height { get; set; }
+        string Weight { get; set; }
+        string Birthday { get; set; }
+        string Matches { get; set; }
+        string Wins { get; set; }
+        string Draws { get; set; }
+        string Losses { get; set; }
+        string Goals { get; set; }
+        string Debut { get; set; }
+        string FormerClubs { get; set; }
+        string Description { get; set; }
+
+        public static List<string> GetDesign(PlayersData player)
+        {
+
+            List<string> playerDescription = new List<string>()
+            {
+                player.Name,
+                "Trøjeponsor: " + player.Sponsor,
+                "Postion: " + player.Posistion,
+                "Højde: " + player.Height,
+                "Vægt: " + player.Weight,
+                "Fødselsdato: " + player.Birthday,
+                "Kampe: " + player.Matches,
+                "Vundne: " + player.Wins,
+                "Uafgjort: " + player.Draws,
+                "Tabte: " + player.Losses,
+                "Mål: " + player.Goals,
+                "Debut: " + player.Debut,
+                "Tidligere klubber: " + player.Former_Clubs,
+                player.Description,
+            };
+            return playerDescription;
+        }
+    }
+}
---
+++ FodboldApp/FodboldApp/FodboldApp/Colorization/ColoringLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FodboldApp.Colorization
+{
+    class ColoringLogic
+    {
+        private static Color Primary = Color.FromHex("#ffffff");
+        private static Color Secondary = Color.FromHex("#f0f0f0");
+
+        public static void InitColors(int i)
+        {
+            switch(i)
+            {
+
+            }
+        }
+
+        public static Color GetColor(int i)
+        {
+            if (i % 2 == 0) return Primary;
+            return Secondary;
+        }
+    }
+}
