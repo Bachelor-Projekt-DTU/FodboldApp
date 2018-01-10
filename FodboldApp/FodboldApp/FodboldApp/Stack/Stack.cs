ffd07cf9b9b821ce885c9aed7fd44ca68e63243a
Olivier Bonde Petersen <osbpetersen@gmail.com>
2018-01-10 10:10:29 +0100

    Stack class setup

---
+++ FodboldApp/FodboldApp/FodboldApp/Stack/Stack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FodboldApp.Stack
+{
+    class Stack
+    {
+        NavigationPage NewsContent;
+        NavigationPage PlayerContent;
+        NavigationPage MatchContent;
+        NavigationPage TournamentContent;
+        NavigationPage HistoryContent;
+
+        public Stack()
+        {
+            NewsContent = new NavigationPage(new News());
+            PlayerContent = new NavigationPage(new Players());
+            MatchContent = new NavigationPage(new Matches());
+            TournamentContent = new NavigationPage(new Tournament());
+            HistoryContent = new NavigationPage(new History());
+        }
+    }
+}
