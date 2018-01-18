c79eea96e5ddd457c9b21cd6e19b50d7b6c7425f
Hadi <hadihorani@hotmail.com>
2018-01-18 15:42:01 +0100

    Match page adjustments. Added comment button

---
+++ FodboldApp/FodboldApp/FodboldApp/Model/CommentsData.cs
@@ -0,0 +1,10 @@
+﻿namespace FodboldApp.Model
+{
+    class CommentsData
+    {
+        public string ImageURL { get; set; }
+        public string UserName { get; set; }
+        public string UserComment { get; set; }
+        public string UserId { get; set; }
+    }
+}
