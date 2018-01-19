3da8e8b55e6b32cf9fb08951f6c111c2b5af5923
Olivier Bonde Petersen <osbpetersen@gmail.com>
2018-01-19 13:03:41 +0100

    PlayerDescription MVVM

---
+++ FodboldApp/FodboldApp/FodboldApp/Model/MatchModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FodboldApp.Model
+{
+    class MatchModel
+    {
+        public string Teams { get; set; }
+        public string Score { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
