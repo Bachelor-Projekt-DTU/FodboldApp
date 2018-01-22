91e8f05d7dfb3ac467f6bfea1e7cba2db50d7323
Hadi <hadihorani@hotmail.com>
2018-01-22 16:55:47 +0100

    Better name adjustments for folders and classes

---
+++ FodboldApp/FodboldApp/FodboldApp/View/XamlTemplate/MatchHeaderTemplate.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FodboldApp.View.XamlTemplate
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MatchHeaderTemplate : StackLayout
+	{
+		public MatchHeaderTemplate()
+		{
+			InitializeComponent();
+		}
+	}
+}
