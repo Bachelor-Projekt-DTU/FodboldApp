ad5ca554cc80d39d202af679a720b8e17d6d86a7
Hadi <hadihorani@hotmail.com>
2018-02-23 16:27:36 +0100

    Adjusted the PlayerModel

--- FodboldApp/Model/Model/PlayerModel.cs
+++ FodboldApp/Model/Model/PlayerModel.cs
@@ -1,30 +1,27 @@
 ﻿using Realms;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
 
 namespace FodboldApp.Model
 {
-    public class PlayerModel : RealmObject
+    public class PlayersModel : RealmObject
     {
         public string ImageURL { get; set; }
         public string Sponsor { get; set; }
-        public int Height { get; set; }
-        public int Weight { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
         public string Birthday { get; set; }
-        public int Matches { get; set; }
-        public int Wins { get; set; }
-        public int Draws { get; set; }
-        public int Losses { get; set; }
-        public int Goals { get; set; }
-        public int Assists { get; set; }
-        public int MVP { get; set; }
-        public int Clean_Sheet { get; set; }
+        public string Matches { get; set; }
+        public string Wins { get; set; }
+        public string Draws { get; set; }
+        public string Losses { get; set; }
+        public string Goals { get; set; }
+        public string Assists { get; set; }
+        public string MVP { get; set; }
+        public string Clean_Sheet { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
         public string Debut { get; set; }
         public string Former_Clubs { get; set; }
         public string Description { get; set; }
+        public string InternationalMatches { get; set; }
     }
 }
