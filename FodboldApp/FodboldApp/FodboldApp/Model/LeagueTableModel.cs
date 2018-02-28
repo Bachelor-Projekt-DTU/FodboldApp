760f52267cbc58a06cf1857ec706574011749112
Hadi <hadihorani@hotmail.com>
2018-02-28 12:21:31 +0100

    Model for League Table added

---
+++ FodboldApp/FodboldApp/FodboldApp/Model/LeagueTableModel.cs
@@ -0,0 +1,20 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FodboldApp.Model
+{
+    class LeagueTableModel : RealmObject
+    {
+        public string Position { get; set; }
+        public string Team { get; set; }
+        public string MP { get; set; }
+        public string Wins { get; set; }
+        public string Draws { get; set; }
+        public string Losses { get; set; }
+        public string GoalsFor { get; set; }
+        public string GoalsAgainst { get; set; }
+        public string Points { get; set; }
+    }
+}
---
+++ FodboldApp/Model/Model/LeagueTableModel.cs
@@ -0,0 +1,17 @@
+﻿using Realms;
+
+namespace Model.Model
+{
+    class LeagueTableModel : RealmObject
+    {
+        public string Position { get; set; }
+        public string Team { get; set; }
+        public string MP { get; set; }
+        public string Wins { get; set; }
+        public string Draws { get; set; }
+        public string Losses { get; set; }
+        public string GoalsFor { get; set; }
+        public string GoalsAgainst { get; set; }
+        public string Points { get; set; }
+    }
+}
