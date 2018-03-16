f576b955528a8d11208a9fd519c7361b6426bc97
Hadi <hadihorani@hotmail.com>
2018-03-16 21:34:14 +0100

    Style fix

--- FodboldApp/FodboldApp/FodboldApp/ViewModel/HistoricalStandingVM.cs
+++ FodboldApp/FodboldApp/FodboldApp/ViewModel/HistoricalStandingVM.cs
@@ -1,7 +1,6 @@
 ﻿using FodboldApp.Model;
 using Realms;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
